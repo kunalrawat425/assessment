2ec9ebfb50cbc7a1acc50bb6f0ff681d9e23e815
kunalrawat425 <kunalrawat425@gmail.com>
2023-11-05 14:13:45 +0530

    frontend added

---
+++ frontend/src/layouts/dashboard/config-navigation.jsx
@@ -0,0 +1,18 @@
+import SvgColor from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => (
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const navConfig = [
+  {
+    title: 'movies',
+    path: '/movies',
+    icon: icon('ic_user'),
+  },
+
+];
+
+export default navConfig;
---
+++ frontend/src/routes/sections.jsx
@@ -0,0 +1,47 @@
+import { lazy, Suspense } from 'react';
+import { Outlet, Navigate, useRoutes } from 'react-router-dom';
+
+import DashboardLayout from 'src/layouts/dashboard';
+
+export const MoviePage= lazy(() => import('src/pages/movie'));
+export const LoginPage = lazy(() => import('src/pages/login'));
+export const RegisterPage = lazy(() => import('src/pages/register'));
+export const Page404 = lazy(() => import('src/pages/page-not-found'));
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  const routes = useRoutes([
+    {
+      element: (
+        <DashboardLayout>
+          <Suspense>
+            <Outlet />
+          </Suspense>
+        </DashboardLayout>
+      ),
+      children: [
+        { element: <MoviePage/>, index: true },
+        { path: 'movies', element: <MoviePage/> },
+      ],
+    },
+    {
+      path: 'login',
+      element: <LoginPage />,
+    },
+    {
+      path: 'register',
+      element: <RegisterPage />,
+    },
+    {
+      path: '404',
+      element: <Page404 />,
+    },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
+  ]);
+
+  return routes;
+}
